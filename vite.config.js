7f8bd3cac6038f5fc23d990febb7eac8de834c97
こぴぺたん <67095323+c-a-p-engineer@users.noreply.github.com>
2026-08-06 01:30:54 +0900

    build: include balloon attack page

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,8 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html")
+        main: resolve(__dirname, "src/index.html"),
+        balloonAttack: resolve(__dirname, "src/balloon-attack/index.html")
       }
     }
   }
