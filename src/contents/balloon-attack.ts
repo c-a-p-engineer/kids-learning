ceb345264d506c9b30a3ec4e4972b953bef70c67
こぴぺたん <67095323+c-a-p-engineer@users.noreply.github.com>
2026-08-06 01:28:48 +0900

    feat: add balloon attack content metadata

---
+++ src/contents/balloon-attack.ts
@@ -0,0 +1,8 @@
+import type { LearningContent } from "../app/types";
+
+export const BALLOON_ATTACK_CONTENT: LearningContent = {
+  id: "balloon-attack",
+  title: "30びょう バルーンアタック",
+  description: "カメラのまえで うごいて、ふうせんを たくさん わろう",
+  tags: ["うんどう", "カメラ", "1さいから", "30びょう"],
+};
